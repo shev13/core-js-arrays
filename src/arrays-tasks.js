672aabb2b06ee1426a5ce7548ff40b651302d727
Victor Shevchenko <13shev.js@gmail.com>
2024-01-03 00:16:29 +0300

    feat: implement getFalsyValuesCount function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -377,8 +377,11 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((acc, item) => {
+    if (item) return acc;
+    return acc + 1;
+  }, 0);
 }
 
 /**
