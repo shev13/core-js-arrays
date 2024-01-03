ea1f35b6f7db3d5fe2b90adf08b19ed20393e895
Victor Shevchenko <13shev.js@gmail.com>
2024-01-03 23:28:54 +0300

    feat: implement sumArrays function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -40,8 +40,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length > arr2.length)
+    return arr1.map(
+      (item, index) => item + (arr2[index] === undefined ? 0 : arr2[index])
+    );
+  return arr2.map(
+    (item, index) => item + (arr1[index] === undefined ? 0 : arr1[index])
+  );
 }
 
 /**
