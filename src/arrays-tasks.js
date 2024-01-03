50227012c767af16aa339ede6fc5efa645090cbc
Victor Shevchenko <13shev.js@gmail.com>
2024-01-03 23:38:46 +0300

    feat: implement isSameLength function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -146,8 +146,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((item, index, array) => item.length === array[0].length);
 }
 
 /**
