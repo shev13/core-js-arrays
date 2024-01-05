54e1ce5cecb18c29b4ad7d0f69461b3691572dee
Victor Shevchenko <13shev.js@gmail.com>
2024-01-05 23:22:30 +0300

    feat: implement getIndicesOfOddNumbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -429,8 +429,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((item, index) => (item % 2 ? index : false))
+    .filter((item) => item !== false);
 }
 
 /**
