3ef4377310f6fea32debad28a771bcd5a1e8a5f1
Victor Shevchenko <13shev.js@gmail.com>
2024-01-06 00:21:08 +0300

    feat: implement findCommonElements function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -481,8 +481,13 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const newArr = [];
+  arr1.map((item) => {
+    if (arr2.includes(item)) newArr.push(item);
+    return 1;
+  });
+  return newArr;
 }
 
 /**
